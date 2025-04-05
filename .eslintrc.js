1f8c5eb6ad5a00a20ab71bc770869d5b0385fa08
psbvision <punitsinghbisht1529@gmail.com>
2025-04-05 20:13:10 +0530

    haar chuka hu mai

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: 'next/core-web-vitals',
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { 
+      argsIgnorePattern: '^_', 
+      varsIgnorePattern: '^_' 
+    }],
+    'react-hooks/exhaustive-deps': 'warn',
+    '@typescript-eslint/no-unused-expressions': 'warn',
+  },
+};
